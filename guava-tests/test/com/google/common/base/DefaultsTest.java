530aea5d0d85eded6e7bd4817ae5658ae84b6861
vonzhou <vonzhou@163.com>
2017-09-09 11:30:58 +0800

    基本类型默认值 Defaults

--- guava-tests/test/com/google/common/base/DefaultsTest.java
+++ guava-tests/test/com/google/common/base/DefaultsTest.java
@@ -24,16 +24,16 @@ import junit.framework.TestCase;
  * @author Jige Yu
  */
 public class DefaultsTest extends TestCase {
-  public void testGetDefaultValue() {
-    assertEquals(false, Defaults.defaultValue(boolean.class).booleanValue());
-    assertEquals('\0', Defaults.defaultValue(char.class).charValue());
-    assertEquals(0, Defaults.defaultValue(byte.class).byteValue());
-    assertEquals(0, Defaults.defaultValue(short.class).shortValue());
-    assertEquals(0, Defaults.defaultValue(int.class).intValue());
-    assertEquals(0, Defaults.defaultValue(long.class).longValue());
-    assertEquals(0.0f, Defaults.defaultValue(float.class).floatValue());
-    assertEquals(0.0d, Defaults.defaultValue(double.class).doubleValue());
-    assertNull(Defaults.defaultValue(void.class));
-    assertNull(Defaults.defaultValue(String.class));
-  }
+    public void testGetDefaultValue() {
+        assertEquals(false, Defaults.defaultValue(boolean.class).booleanValue());
+        assertEquals('\0', Defaults.defaultValue(char.class).charValue());
+        assertEquals(0, Defaults.defaultValue(byte.class).byteValue());
+        assertEquals(0, Defaults.defaultValue(short.class).shortValue());
+        assertEquals(0, Defaults.defaultValue(int.class).intValue());
+        assertEquals(0, Defaults.defaultValue(long.class).longValue());
+        assertEquals(0.0f, Defaults.defaultValue(float.class).floatValue());
+        assertEquals(0.0d, Defaults.defaultValue(double.class).doubleValue());
+        assertNull(Defaults.defaultValue(void.class));
+        assertNull(Defaults.defaultValue(String.class));
+    }
 }
--- guava/src/com/google/common/base/Defaults.java
+++ guava/src/com/google/common/base/Defaults.java
@@ -17,6 +17,7 @@ package com.google.common.base;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.annotations.GwtIncompatible;
+
 import javax.annotation.Nullable;
 
 /**
@@ -27,38 +28,39 @@ import javax.annotation.Nullable;
  */
 @GwtIncompatible
 public final class Defaults {
-  private Defaults() {}
-
-  private static final Double DOUBLE_DEFAULT = Double.valueOf(0d);
-  private static final Float FLOAT_DEFAULT = Float.valueOf(0f);
-
-  /**
-   * Returns the default value of {@code type} as defined by JLS --- {@code 0} for numbers, {@code
-   * false} for {@code boolean} and {@code '\0'} for {@code char}. For non-primitive types and
-   * {@code void}, {@code null} is returned.
-   */
-  @Nullable
-  @SuppressWarnings("unchecked")
-  public static <T> T defaultValue(Class<T> type) {
-    checkNotNull(type);
-    if (type == boolean.class) {
-      return (T) Boolean.FALSE;
-    } else if (type == char.class) {
-      return (T) Character.valueOf('\0');
-    } else if (type == byte.class) {
-      return (T) Byte.valueOf((byte) 0);
-    } else if (type == short.class) {
-      return (T) Short.valueOf((short) 0);
-    } else if (type == int.class) {
-      return (T) Integer.valueOf(0);
-    } else if (type == long.class) {
-      return (T) Long.valueOf(0L);
-    } else if (type == float.class) {
-      return (T) FLOAT_DEFAULT;
-    } else if (type == double.class) {
-      return (T) DOUBLE_DEFAULT;
-    } else {
-      return null;
+    private Defaults() {
     }
-  }
+
+    private static final Double DOUBLE_DEFAULT = Double.valueOf(0d);
+    private static final Float FLOAT_DEFAULT = Float.valueOf(0f);
+
+    /**
+     * Returns the default value of {@code type} as defined by JLS --- {@code 0} for numbers, {@code
+     * false} for {@code boolean} and {@code '\0'} for {@code char}. For non-primitive types and
+     * {@code void}, {@code null} is returned.
+     */
+    @Nullable
+    @SuppressWarnings("unchecked")
+    public static <T> T defaultValue(Class<T> type) {
+        checkNotNull(type);
+        if (type == boolean.class) {
+            return (T) Boolean.FALSE;
+        } else if (type == char.class) {
+            return (T) Character.valueOf('\0');
+        } else if (type == byte.class) {
+            return (T) Byte.valueOf((byte) 0);
+        } else if (type == short.class) {
+            return (T) Short.valueOf((short) 0);
+        } else if (type == int.class) {
+            return (T) Integer.valueOf(0);
+        } else if (type == long.class) {
+            return (T) Long.valueOf(0L);
+        } else if (type == float.class) {
+            return (T) FLOAT_DEFAULT;
+        } else if (type == double.class) {
+            return (T) DOUBLE_DEFAULT;
+        } else {
+            return null;
+        }
+    }
 }
