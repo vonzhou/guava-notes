9533ba1297227a9d2f4fbaf60a8a357318fdf639
vonzhou <vonzhou@163.com>
2017-09-09 10:47:04 +0800

    秒表 Stopwatch

--- guava-testlib/src/com/google/common/testing/FakeTicker.java
+++ guava-testlib/src/com/google/common/testing/FakeTicker.java
@@ -21,6 +21,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.base.Ticker;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -30,7 +31,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * The ticker can be configured so that the time is incremented whenever {@link #read} is called:
  * see {@link #setAutoIncrementStep}.
  * <p>
- * This class is thread-safe.
+ * This class is thread-safe.  线程安全
  *
  * @author Jige Yu
  * @since 10.0
@@ -39,33 +40,38 @@ import java.util.concurrent.atomic.AtomicLong;
 @GwtCompatible
 public class FakeTicker extends Ticker {
 
-  private final AtomicLong nanos = new AtomicLong();
-  private volatile long autoIncrementStepNanos;
+    private final AtomicLong nanos = new AtomicLong();
+    private volatile long autoIncrementStepNanos;
 
-  /** Advances the ticker value by {@code time} in {@code timeUnit}. */
-  public FakeTicker advance(long time, TimeUnit timeUnit) {
-    return advance(timeUnit.toNanos(time));
-  }
+    /**
+     * Advances the ticker value by {@code time} in {@code timeUnit}.
+     */
+    public FakeTicker advance(long time, TimeUnit timeUnit) {
+        return advance(timeUnit.toNanos(time));
+    }
 
-  /** Advances the ticker value by {@code nanoseconds}. */
-  public FakeTicker advance(long nanoseconds) {
-    nanos.addAndGet(nanoseconds);
-    return this;
-  }
+    /**
+     * Advances the ticker value by {@code nanoseconds}.
+     */
+    public FakeTicker advance(long nanoseconds) {
+        nanos.addAndGet(nanoseconds);
+        return this;
+    }
 
-  /**
-   * Sets the increment applied to the ticker whenever it is queried.
-   *
-   * <p>The default behavior is to auto increment by zero. i.e: The ticker is left unchanged when
-   * queried.
-   */
-  public FakeTicker setAutoIncrementStep(long autoIncrementStep, TimeUnit timeUnit) {
-    checkArgument(autoIncrementStep >= 0, "May not auto-increment by a negative amount");
-    this.autoIncrementStepNanos = timeUnit.toNanos(autoIncrementStep);
-    return this;
-  }
+    /**
+     * Sets the increment applied to the ticker whenever it is queried.
+     * <p>
+     * <p>The default behavior is to auto increment by zero. i.e: The ticker is left unchanged when
+     * queried.
+     */
+    public FakeTicker setAutoIncrementStep(long autoIncrementStep, TimeUnit timeUnit) {
+        checkArgument(autoIncrementStep >= 0, "May not auto-increment by a negative amount");
+        this.autoIncrementStepNanos = timeUnit.toNanos(autoIncrementStep);
+        return this;
+    }
 
-  @Override public long read() {
-    return nanos.getAndAdd(autoIncrementStepNanos);
-  }
+    @Override
+    public long read() {
+        return nanos.getAndAdd(autoIncrementStepNanos);
+    }
 }
